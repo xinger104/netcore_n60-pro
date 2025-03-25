c1794d6d8ef475d56ea0eb87f53521dfe0fa4f76
moshanghuakai001 <hnlylsb@gmail.com>
2025-03-25 14:46:33 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -21,4 +21,5 @@
 
 # Remove print samba4
 sed -i '/printer/d' target/linux/mediatek/image/mt7986.mk
+sed -i 's/kmod-usb-net-rndis //g' target/linux/mediatek/image/mt7986.mk
 sed -i 's/luci-app-samba4//g' target/linux/mediatek/image/mt7986.mk
