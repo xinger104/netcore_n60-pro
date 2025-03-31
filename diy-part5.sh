29f7a526f93238d1345b8a6e7673e716fbdf3077
moshanghuakai001 <hnlylsb@gmail.com>
2025-03-31 11:44:14 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -22,7 +22,7 @@
 # Remove print samba4
 sed -i '/printer/d' target/linux/mediatek/image/mt7986.mk
 sed -i 's/kmod-usb-net-rndis //g' target/linux/mediatek/image/mt7986.mk
-sed -i 's/ luci-app-samba4//g' target/linux/mediatek/image/mt7986.mk
+sed -i '/ksmbd/d' target/linux/mediatek/image/mt7986.mk
 
 # 添加组播防火墙规则
 cat >> package/network/config/firewall/files/firewall.config <<EOF
