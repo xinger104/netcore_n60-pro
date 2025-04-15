5d86454a0cd8700f219d36c3298ff0999c1f062e
moshanghuakai001 <hnlylsb@gmail.com>
2025-04-15 10:34:30 +0800

    Update diy-part3.sh
    dailook-hanwckf

--- diy-part3.sh
+++ diy-part3.sh
@@ -26,7 +26,6 @@ sed -i 's/ luci-app-samba4//g' target/linux/mediatek/image/mt7986.mk
 
 # 添加组播防火墙规则
 cat >> package/network/config/firewall/files/firewall.config <<EOF
-
 config rule
         option name 'Allow-UDP-igmpproxy'
         option src 'wan'
